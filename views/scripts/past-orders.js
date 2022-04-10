050d7eb728e5149b121bd1677b657792df21c9a7
Eugene Helland <eugenehelland@mail.com>
2022-04-10 15:32:37 -0600

    "PAST ORDERS" button displays the the past 2 orders for the current user, this is currently generated from a hard-coded JS object.

--- views/scripts/past-orders.js
+++ views/scripts/past-orders.js
@@ -2,18 +2,56 @@ const pastOrdersBtn = document.querySelector('.past-orders');
 const menuArea = document.querySelector('.menu');
 
 const ordersObj = {
-  email: 'customer@one.com',
+  customerName: 'First Last',
   orders: [
     {
       dish: 'House Special Pho',
       quantity: 1,
       specialInstructions: 'No onions or basil please.'    
     },
-    
+    {
+      dish: 'Pho Lasagna',
+      quantity: 2,
+      specialInstructions: ''
+    }
   ]
 }
 
 
 pastOrdersBtn.addEventListener('click', () => {
+  // Clear menu items
   menuArea.innerHTML = '';
+
+  let pastOrdersDiv = document.createElement('div');
+  pastOrdersDiv.classList.add('past-orders');
+
+  let pastOrdersTitle = document.createElement('h3');
+  pastOrdersTitle.textContent = `Past orders for: ${ordersObj.customerName}`;
+
+  // List that will display the past orders
+  let displayOrders = document.createElement('ul');
+
+  for (let order of ordersObj.orders) {
+    let displayOrder = document.createElement('li');
+    console.log('DISPLAYORDER: ', displayOrder);
+
+    let orderDetails = `
+      <span>
+        ${order.dish}
+      </span>
+      <span>
+        ${order.quantity}
+      </span>
+      <span>
+        ${order.specialInstructions}
+      </span>
+    `;
+
+    displayOrder.innerHTML = orderDetails;
+    displayOrders.append(displayOrder);
+  }
+
+  pastOrdersDiv.append(pastOrdersTitle);
+  pastOrdersDiv.append(displayOrders);
+  menuArea.append(pastOrdersDiv);
 });
