3241e10eb1a8279eb035abecbd6fdc0a8815ab8d
Eugene Helland <eugenehelland@mail.com>
2022-04-11 11:42:53 -0600

    Adjust HTML generated from generate-menu.js

--- views/scripts/generate-menu.js
+++ views/scripts/generate-menu.js
@@ -12,7 +12,43 @@ const menuContent = [
     name: "Pho Lasagna",
     price: "$15",
     caption: "Fusion!",
-  }
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIKjlrsxzNTgZ_ocm-hjeJuFtVL3Ug1asjw&usqp=CAU",
+    name: "Pho Lasagna",
+    price: "$15",
+    caption: "Fusion!",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIKjlrsxzNTgZ_ocm-hjeJuFtVL3Ug1asjw&usqp=CAU",
+    name: "Pho Lasagna",
+    price: "$15",
+    caption: "Fusion!",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIKjlrsxzNTgZ_ocm-hjeJuFtVL3Ug1asjw&usqp=CAU",
+    name: "Pho Lasagna",
+    price: "$15",
+    caption: "Fusion!",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIKjlrsxzNTgZ_ocm-hjeJuFtVL3Ug1asjw&usqp=CAU",
+    name: "Pho Lasagna",
+    price: "$15",
+    caption: "Fusion!",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIKjlrsxzNTgZ_ocm-hjeJuFtVL3Ug1asjw&usqp=CAU",
+    name: "Pho Lasagna",
+    price: "$15",
+    caption: "Fusion!",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbIKjlrsxzNTgZ_ocm-hjeJuFtVL3Ug1asjw&usqp=CAU",
+    name: "Pho Lasagna",
+    price: "$15",
+    caption: "Fusion!",
+  },
 ];
 
 for (let menuItem of menuContent) {
@@ -21,11 +57,18 @@ for (let menuItem of menuContent) {
   singleMenu.classList.add('single-menu');
 
   let menuItemHTML = `
-    <img src="${menuItem.img} alt="picture of ${menuItem.name}"/>
+    <div class="image-container">
+      <img src="${menuItem.img} alt="picture of ${menuItem.name}"/>
+    </div>
     <div class="menu-content">
       <h4>${menuItem.name}<span>${menuItem.price}<span></h4>
-      <p>${menuItem.caption}<p>
-    </div>   
+      <p>${menuItem.caption}</p>
+      </div>
+    <div style="width: 10%"></div>
+    <div class="button-container">
+      <button>Add To Cart</button>
+    </div>
+  
   `;
   
   singleMenu.innerHTML = menuItemHTML;
