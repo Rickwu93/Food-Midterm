20158f7082cc4ef1656672043937b318569bdf5e
Lucus Riccioni <lucusroni@gmail.com>
2022-04-15 16:46:00 +0000

    Code refactor and bug fixes

--- public/scripts/menuCategories.js
+++ public/scripts/menuCategories.js
@@ -1,51 +1,42 @@
-const getCategories = function () {
-
- // event listeners for menu category tab clicks
-
 $(() => {
 
 // event listeners for menu category tab clicks
 
+const container = $('.menu');
+
   $('.mains').on('click', function() {
-    const container = $('.menu');
     const item = $('.main');
-    const target = item.offset().top - container.offset().top + container.scrollTop();
+    const target = item.offset().top - container.offset().top;
     container.scrollTop(target);
   });
 
   $('.soups').on('click', function() {
-    const container = $('.menu');
     const item = $('.soup');
-    const target = item.offset().top - container.offset().top + container.scrollTop();
+    const target = item.offset().top - container.offset().top + container.scrollTop() -100;
     container.scrollTop(target);
   });
 
   $('.sandwiches').on('click', function() {
-    const container = $('.menu');
     const item = $('.sandwich');
-    const target = item.offset().top - container.offset().top + container.scrollTop();
+    const target = item.offset().top - container.offset().top + container.scrollTop() - 100;
     container.scrollTop(target);
   });
 
   $('.drinks').on('click', function() {
-    const container = $('.menu');
     const item = $('.drink');
-    const target = item.offset().top - container.offset().top + container.scrollTop();
+    const target = item.offset().top - container.offset().top + container.scrollTop() - 100;
     container.scrollTop(target);
   });
 
   $('.desserts').on('click', function() {
-    const container = $('.menu');
     const item = $('.dessert');
-    const target = item.offset().top - container.offset().top + container.scrollTop();
+    const target = item.offset().top - container.offset().top + container.scrollTop() - 100;
     container.scrollTop(target);
   });
 
   $('.bubble-tea').on('click', function() {
-    const container = $('.menu');
     const item = $('.bubbletea');
-    const target = item.offset().top - container.offset().top + container.scrollTop();
+    const target = item.offset().top - container.offset().top + container.scrollTop() - 100;
     container.scrollTop(target);
   });
 });
-}
